4c05460d4d4d2ab237de38eaa3687b58e6034a79
Isabel Rojo Herrera <isabelc.rojoherrera@gmail.com>
2024-03-13 22:12:27 +0000

    se modifica el redondeo

--- HackerRank/GradingStudents.java
+++ HackerRank/GradingStudents.java
@@ -5,25 +5,32 @@ public class GradingStudents {
 
     public List<Integer> gradingStudents(List<Integer> grades) {
 
-
         for(int i=0; i<grades.size(); i++){
             grades.set(i, round(grades.get(i)));
         }
-    
         return grades;
-
+        
     }
 
     public Integer round(Integer grade){
 
-        if(grade % 5 <3 )
-            return grade + (grade%5);
-
+        if(grade > 38 && (5-(grade%5)) <3 )
+            return grade + (5-(grade%5));
         return grade;
     }
 
 
     public static void main (String a []){
+
+        GradingStudents g = new GradingStudents();
+        List <Integer> list = new ArrayList<>();
+
+        list.add(73);
+        list.add(67);
+        list.add(38);
+        list.add(33);
+        
+        System.out.println(g.gradingStudents(list));
 
     }
 }
